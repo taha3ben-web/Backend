9e7d225916aadf6acbc81bfe2ef7f0cc2eb7827a
taha3ben-web <taha3ben@gmail.com>
2026-07-28 08:34:21 +0100

    refactor(financial): استخراج محرّك دفتر الأستاذ إلى LedgerCoreService (المرحلة 6، بلا تغيير سلوك)

--- src/modules/financial/financial.module.ts
+++ src/modules/financial/financial.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { FinancialService } from "./financial.service";
+import { LedgerCoreService } from "./ledger-core.service";
 import { FinancialController } from "./financial.controller";
 import { CountryConfigModule } from "../country-config/country-config.module";
 
 @Module({
   imports: [CountryConfigModule],
   controllers: [FinancialController],
-  providers: [FinancialService],
-  exports: [FinancialService],
+  providers: [LedgerCoreService, FinancialService],
+  exports: [FinancialService, LedgerCoreService],
 })
 export class FinancialModule {}
